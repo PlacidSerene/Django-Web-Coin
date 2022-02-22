3e759e7ddbca77ae75c356404a7d8d666e618afb
Daniel <t.dung.lhp@gmail.com>
2022-02-21 17:00:02 -0800

    registration

--- crypto/coins/urls.py
+++ crypto/coins/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('test', views.test, name='test'),
     path('login',views.login_view, name='login'),
     path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
 ]
--- crypto/coins/views.py
+++ crypto/coins/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
+from django.db import IntegrityError
 # Create your views here.
 import requests
 # from .data_processing import get_data
 from plotly.offline import plot
 import plotly.graph_objects as go
+
+# model
+from .models import User
 
 def index(request):
     response = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C3d%2C7d%2C30d')
@@ -37,6 +41,31 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "coins/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "coins/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "coins/register.html")
 
 def test(request):
     # Generating some data for plots.
